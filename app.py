546bda24a5cd1ff70ca32f41ac7fefebd415120c
Vadim Gartman <vadim.gartman@yandex.ru>
2022-02-23 22:42:33 +0300

    backend: learn how to handle requests

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", 'Первое приложение', "Обратная связь"]
+menu = [
+    {"name": "Установка", "url": "install-flask"},
+    {"name": "Первое приложение", "url": "first-app"},
+    {"name": "Обратная связь", "url": "contact"},
+]
 
 
 @app.route('/')
@@ -25,6 +29,19 @@ def about():
 def profile(username, user_id, any_path):
     print(url_for('about'))
     return f"Пользователь: {username} с id = {user_id}, перешел по пути {any_path}"
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():  # put application's code here
+
+    if request.method == "POST":
+        print(request.form)
+
+    return render_template(
+        'contact.html',
+        title="Обратная связь",
+        menu=menu
+    )
 
 
 if __name__ == '__main__':
