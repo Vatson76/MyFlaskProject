631f70c14db5c2df6cbf4e4f3381ffa7eb94a44c
Vadim Gartman <vadim.gartman@yandex.ru>
2022-02-23 21:57:51 +0300

    backend: learn how url_for works and how flask url builds urls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,8 @@ menu = ["Установка", 'Первое приложение', "Обратн
 
 
 @app.route('/')
-def index():  # put application's code here
+def index():
+    print(url_for('index'))
     return render_template(
         'index.html',
         title='Про Flask',
@@ -15,8 +16,16 @@ def index():  # put application's code here
 
 
 @app.route('/about')
-def about():  # put application's code here
+def about():
+    print(url_for('about'))
     return render_template('about.html')
+
+
+@app.route('/profile/<username>/<int:user_id>/<path:any_path>')
+def profile(username, user_id, any_path):
+    print(url_for('about'))
+    return f"Пользователь: {username} с id = {user_id}, перешел по пути {any_path}"
+
 
 if __name__ == '__main__':
     app.run()
